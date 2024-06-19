562be90e203fb6b33677f53528c5c9c12a293af9
irsad8 <irsadida.08@gmail.com>
2024-06-19 21:50:51 +0700

    commit

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,6 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+test('cek', () => {
+  expect(2+3).toBe(5);
+});
